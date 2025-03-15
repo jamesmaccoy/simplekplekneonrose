0d1353e34ec7a5fdf7af03acc783d5016a891254
jamesmaccoy <thankyou.digital@gmail.com>
2025-03-15 16:05:43 +0200

    vibing

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,6 +1,7 @@
 // storage-adapter-import-placeholder
 import { vercelPostgresAdapter } from '@payloadcms/db-vercel-postgres'
-
+import { s3Storage } from '@payloadcms/storage-s3'
+//import { cloudStorage, r2Adapter } from '@payloadcms/storage-r2'
 
 import sharp from 'sharp' // sharp-import
 import path from 'path'
@@ -71,6 +72,20 @@ export default buildConfig({
   globals: [Header, Footer],
   plugins: [
     ...plugins,
+    s3Storage({
+      bucket: process.env.R2_BUCKET ?? 'default-bucket',
+      collections: {
+        media: true,
+      },
+      config: {
+        credentials: {
+          accessKeyId: process.env.R2_ACCESS_KEY_ID ?? 'default-access-key',
+          secretAccessKey: process.env.R2_SECRET_ACCESS_KEY ?? 'default-secret-key',
+        },
+        endpoint: process.env.R2_ENDPOINT ?? 'default-endpoint',
+        region: process.env.R2_REGION ?? 'default-region',
+      },
+    }),
     // storage-adapter-placeholder
   ],
   secret: process.env.PAYLOAD_SECRET,
