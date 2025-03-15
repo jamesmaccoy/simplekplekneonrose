680d20a70b70f88564f97883c8da1494b69cac08
jamesmaccoy <thankyou.digital@gmail.com>
2025-03-15 15:43:52 +0200

    Update payload.config.ts

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,6 +1,7 @@
 // storage-adapter-import-placeholder
 import { vercelPostgresAdapter } from '@payloadcms/db-vercel-postgres'
 
+
 import sharp from 'sharp' // sharp-import
 import path from 'path'
 import { buildConfig, PayloadRequest } from 'payload'
